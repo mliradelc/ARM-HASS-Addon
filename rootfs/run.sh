3dcff121fa5fc3a8b88bc9395deb9bc3869686d2
Maximiliano Lira Del Canto <8040628+mliradelc@users.noreply.github.com>
2025-10-25 15:12:56 +0200

    Changes in run.sh

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -7,18 +7,18 @@ mkdir -p /media/arm /media/arm/transcode /media/arm/raw /media/music /media/medi
 
 # Symlink /etc/arm/config to media folder
 rm -rf /etc/arm/config
-ln -s /media/arm/config /etc/arm/config
+ln -s /media/arm/config /share/arm_config
 
 # Copy default ARM config if not present
 if [ ! -f "/media/arm/config/config/arm.yaml" ]; then
-  echo "[INFO] Copying default arm.yaml to /etc/arm/config"
+  echo "[INFO] Copying default arm.yaml to /etc/arm_config"
   cp /defaults/arm.yaml /media/arm/config
 fi
 
 # Copy default ARM Music config if not present
 if [ ! -f "/media/arm/config/config/abcde.conf" ]; then
-  echo "[INFO] Copying default abcde.conf to /etc/arm/config"
-  cp /defaults/abcde.conf /media/arm/config
+  echo "[INFO] Copying default abcde.conf to /share/arm_config"
+  cp /defaults/abcde.conf /share/arm_config
 fi
 
 # Start armui
